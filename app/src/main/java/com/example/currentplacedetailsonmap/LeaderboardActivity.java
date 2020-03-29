3c22945223377acea00820a92cdd9cc5390ab6f9
ryantakeshihagerty <ryanhagerty29@gmail.com>
2020-03-28 22:07:52 -0700

    3rd commit

---
+++ app/src/main/java/com/example/currentplacedetailsonmap/LeaderboardActivity.java
@@ -0,0 +1,11 @@
+package com.example.currentplacedetailsonmap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LeaderboardActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaderboard);
+    }
+}
