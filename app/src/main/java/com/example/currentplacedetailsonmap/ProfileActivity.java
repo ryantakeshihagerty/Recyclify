6e8e3168cea4bd1fbee73e5b6789aa74fa50ed64
ryantakeshihagerty <ryanhagerty29@gmail.com>
2020-03-30 14:24:30 -0700

    succesfully grabbed values for profile

--- app/src/main/java/com/example/currentplacedetailsonmap/ProfileActivity.java
+++ app/src/main/java/com/example/currentplacedetailsonmap/ProfileActivity.java
@@ -2,20 +2,72 @@ package com.example.currentplacedetailsonmap;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.PopupMenu;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class ProfileActivity extends Activity implements PopupMenu.OnMenuItemClickListener {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        getProfileValues();
+        //temp userID until login is implemented
+        String myUserID = "fxie";
+        getProfileValues(myUserID);
     }
 
-    private void getProfileValues() {
+    private void getProfileValues(final String user) {
+        class DownloadProfile extends AsyncTask<Void, Void, String> {
+            @Override
+            protected String doInBackground(Void... voids) {
+                try {
+                    URL url = new URL("https://ineedtophp.000webhostapp.com/get_profile_values.php?userID=" + user);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String json;
+                    while ((json = br.readLine()) != null) {
+                        sb.append(json + "\n"); // Adding to JSON string
+                    }
+                    return sb.toString().trim(); // Returning JSON string
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+                try {
+                    loadValues(result);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        DownloadProfile dlProfile = new DownloadProfile();
+        dlProfile.execute();
+    }
+
+    private void loadValues(String jsonString) throws JSONException {
+        JSONArray jsonArray = new JSONArray(jsonString);
     }
 
     public void showPopup(View v) {
